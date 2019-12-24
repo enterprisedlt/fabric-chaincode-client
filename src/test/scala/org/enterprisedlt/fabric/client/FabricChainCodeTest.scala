a4099decf2cc5c889fa244a4d0f4b8a0d30616e3
Maxim Fedin <m-fedin@mail.ru>
2019-12-24 11:52:22 +0300

    some progress.

--- src/test/scala/org/enterprisedlt/fabric/client/FabricChainCodeTest.scala
+++ src/test/scala/org/enterprisedlt/fabric/client/FabricChainCodeTest.scala
@@ -1,5 +1,6 @@
 package org.enterprisedlt.fabric.client
 
+import org.enterprisedlt.general.gson.TypeNameResolver
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -7,6 +8,17 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class FabricChainCodeTest extends FunSuite {
+    private val NamesResolver = new TypeNameResolver() {
+        override def resolveTypeByName(name: String): Class[_] = if ("dummy" equals name) classOf[Dummy] else throw new IllegalStateException(s"Unexpected class name: $name")
+
+        override def resolveNameByType(clazz: Class[_]): String = "dummy"
+    }
 
 
 }
+
+
+case class Dummy(
+    name: String,
+    value: String
+)
