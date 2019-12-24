47fe4bc9f216f23b640c0a2caf73f24ab7c3e81e
Maxim Fedin <m-fedin@mail.ru>
2019-12-24 11:26:00 +0300

    init(2)

---
+++ src/test/scala/org/enterprisedlt/fabric/client/FabricChainCodeTest.scala
@@ -0,0 +1,12 @@
+package org.enterprisedlt.fabric.client
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class FabricChainCodeTest extends FunSuite {
+
+
+}
