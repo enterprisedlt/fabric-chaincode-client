51e562a40009478dc3a8947ee4e27b60437bb8ce
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2019-12-25 19:13:54 +0300

    test clean up

--- src/test/scala/org/enterprisedlt/fabric/client/FabricChainCodeTest.scala
+++ src/test/scala/org/enterprisedlt/fabric/client/FabricChainCodeTest.scala
@@ -5,7 +5,7 @@ import java.util.concurrent.CompletableFuture
 import com.google.protobuf.ByteString
 import org.enterprisedlt.general.codecs.GsonCodec
 import org.enterprisedlt.general.gson._
-import org.enterprisedlt.spec.{ContractOperation, ContractResult, OperationType}
+import org.enterprisedlt.spec.{BinaryCodec, ContractOperation, ContractResult, OperationType}
 import org.hyperledger.fabric.protos.peer.FabricProposalResponse
 import org.hyperledger.fabric.sdk.transaction.TransactionContext
 import org.hyperledger.fabric.sdk._
@@ -21,28 +21,67 @@ import scala.collection.JavaConverters._
  */
 @RunWith(classOf[JUnitRunner])
 class FabricChainCodeTest extends FunSuite {
-    private val chaincodeId: ChaincodeID = ChaincodeID.newBuilder()
-      .setName("channel")
-      .setVersion("1.0")
-      .setPath("/dev/null").build()
-    private val gsonCodec = GsonCodec()
-    private val typedCodec = GsonCodec(gsonOptions = _.encodeTypes(typeFieldName = "#TYPE#", typeNamesResolver = NamesResolver))
+    private val chainCodeId: ChaincodeID =
+        ChaincodeID.newBuilder()
+          .setName("channel")
+          .setVersion("1.0")
+          .setPath("/dev/null")
+          .build()
+
+    private val vanillaGSONCodec = GsonCodec()
+
     private val NamesResolver = new TypeNameResolver() {
         override def resolveTypeByName(name: String): Class[_] = if ("dummy" equals name) classOf[Dummy] else throw new IllegalStateException(s"Unexpected class name: $name")
 
         override def resolveNameByType(clazz: Class[_]): String = "dummy"
     }
+    private val typedGSONCodec = GsonCodec(gsonOptions = _.encodeTypes(typeFieldName = "#TYPE#", typeNamesResolver = NamesResolver))
+
 
     trait TestContractSpec {
         @ContractOperation(OperationType.Invoke)
-        def testReturnUnit(tokenType: Int): ContractResult[Unit]
+        def testReturnUnit(arg: Int): ContractResult[Unit]
 
         @ContractOperation(OperationType.Invoke)
-        def testReturnDummy(tokenType: Int): ContractResult[Dummy]
+        def testReturnDummy(arg: Int): ContractResult[Dummy]
+
+        @ContractOperation(OperationType.Invoke)
+        def testInvokeReturnLong(arg: Int): Either[String, Long]
 
     }
 
-    test("should return Unit with gson codec") {
+    test("should work with Unit as return type [vanilla GSON]") {
+        val expectedResult: Unit = ()
+        val cc: TestContractSpec = mockChainCodeForInvoke(vanillaGSONCodec, expectedResult).as[TestContractSpec]
+        assert(cc.testReturnUnit(1) == Right(expectedResult))
+    }
+
+    test("should work with Unit as return type [typed GSON]") {
+        val expectedResult: Unit = ()
+        val cc: TestContractSpec = mockChainCodeForInvoke(typedGSONCodec, expectedResult).as[TestContractSpec]
+        assert(cc.testReturnUnit(1) == Right(expectedResult))
+    }
+
+    test("should work with case class as return type [typed GSON]") {
+        val expectedResult = Dummy("x", "y")
+        val cc: TestContractSpec = mockChainCodeForInvoke(typedGSONCodec, expectedResult).as[TestContractSpec]
+        assert(cc.testReturnDummy(1) == Right(expectedResult))
+    }
+
+    test("should work with Long as return type [vanilla GSON]") {
+        val expectedResult: Long = 10000000L
+        val cc: TestContractSpec = mockChainCodeForInvoke(vanillaGSONCodec, expectedResult).as[TestContractSpec]
+        assert(cc.testInvokeReturnLong(1) == Right(expectedResult))
+    }
+
+    test("should work with Long as return type [typed GSON]") {
+        val expectedResult: Long = 10000000L
+        val cc: TestContractSpec = mockChainCodeForInvoke(typedGSONCodec, expectedResult).as[TestContractSpec]
+        assert(cc.testInvokeReturnLong(1) == Right(expectedResult))
+    }
+
+
+    private def mockChainCodeForInvoke(codec: BinaryCodec, result: Any): FabricChainCode = {
         val client = mock(classOf[HFClient])
         val usr = mock(classOf[User])
         when(client.getUserContext).thenReturn(usr)
@@ -53,7 +92,7 @@ class FabricChainCodeTest extends FunSuite {
 
         val propResponse = ProposalResponseFactory.newProposalResponse(txCtx, ChaincodeResponse.Status.SUCCESS.getStatus, "")
         val tranProReq = TransactionProposalRequest.newInstance(usr)
-        val ccResult = gsonCodec.encode(())
+        val ccResult = codec.encode(result)
         val protoResponse = FabricProposalResponse.Response.newBuilder()
           .setPayload(ByteString.copyFrom(ccResult))
           .build()
@@ -65,81 +104,13 @@ class FabricChainCodeTest extends FunSuite {
 
         propResponse.setProposalResponse(proposalResponse)
         val responses = List(propResponse).asJava
-        val cc: TestContractSpec = new FabricChainCode(client, channel, chaincodeId, gsonCodec).as[TestContractSpec]
 
         when(client.newTransactionProposalRequest()).thenReturn(tranProReq)
         when(channel.sendTransactionProposal(tranProReq)).thenReturn(responses)
         when(channel.sendTransaction(responses, usr))
           .thenReturn(CompletableFuture.completedFuture(null.asInstanceOf[BlockEvent#TransactionEvent]))
 
-        assert(cc.testReturnUnit(1) == Right(()))
-    }
-
-    test("should return Unit with typed codec") {
-        val client = mock(classOf[HFClient])
-        val usr = mock(classOf[User])
-        when(client.getUserContext).thenReturn(usr)
-        val channel = mock(classOf[Channel])
-        val txCtx = mock(classOf[TransactionContext])
-        when(txCtx.getTxID).thenReturn("12345")
-        when(txCtx.getChannelID).thenReturn("channel")
-
-        val propResponse = ProposalResponseFactory.newProposalResponse(txCtx, ChaincodeResponse.Status.SUCCESS.getStatus, "")
-        val tranProReq = TransactionProposalRequest.newInstance(usr)
-        val ccResult = typedCodec.encode(())
-        val protoResponse = FabricProposalResponse.Response.newBuilder()
-          .setPayload(ByteString.copyFrom(ccResult))
-          .build()
-
-        val proposalResponse = FabricProposalResponse.ProposalResponse.newBuilder()
-          .setPayload(ByteString.copyFrom(ccResult))
-          .setResponse(protoResponse)
-          .build()
-
-        propResponse.setProposalResponse(proposalResponse)
-        val responses = List(propResponse).asJava
-        val cc: TestContractSpec = new FabricChainCode(client, channel, chaincodeId, typedCodec).as[TestContractSpec]
-
-        when(client.newTransactionProposalRequest()).thenReturn(tranProReq)
-        when(channel.sendTransactionProposal(tranProReq)).thenReturn(responses)
-        when(channel.sendTransaction(responses, usr))
-          .thenReturn(CompletableFuture.completedFuture(null.asInstanceOf[BlockEvent#TransactionEvent]))
-
-        assert(cc.testReturnUnit(1) == Right(()))
-    }
-
-    test("should return Dummy asset with typed codec") {
-        val client = mock(classOf[HFClient])
-        val usr = mock(classOf[User])
-        when(client.getUserContext).thenReturn(usr)
-        val channel = mock(classOf[Channel])
-        val txCtx = mock(classOf[TransactionContext])
-        when(txCtx.getTxID).thenReturn("12345")
-        when(txCtx.getChannelID).thenReturn("channel")
-
-        val propResponse = ProposalResponseFactory.newProposalResponse(txCtx, ChaincodeResponse.Status.SUCCESS.getStatus, "")
-        val tranProReq = TransactionProposalRequest.newInstance(usr)
-        val dummy = Dummy("x","y")
-        val ccResult = typedCodec.encode(dummy)
-        val protoResponse = FabricProposalResponse.Response.newBuilder()
-          .setPayload(ByteString.copyFrom(ccResult))
-          .build()
-
-        val proposalResponse = FabricProposalResponse.ProposalResponse.newBuilder()
-          .setPayload(ByteString.copyFrom(ccResult))
-          .setResponse(protoResponse)
-          .build()
-
-        propResponse.setProposalResponse(proposalResponse)
-        val responses = List(propResponse).asJava
-        val cc: TestContractSpec = new FabricChainCode(client, channel, chaincodeId, typedCodec).as[TestContractSpec]
-
-        when(client.newTransactionProposalRequest()).thenReturn(tranProReq)
-        when(channel.sendTransactionProposal(tranProReq)).thenReturn(responses)
-        when(channel.sendTransaction(responses, usr))
-          .thenReturn(CompletableFuture.completedFuture(null.asInstanceOf[BlockEvent#TransactionEvent]))
-
-        assert(cc.testReturnDummy(1) == Right(Dummy("x","y")))
+        new FabricChainCode(client, channel, chainCodeId, codec)
     }
 }
 
