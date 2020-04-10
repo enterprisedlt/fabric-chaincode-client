61dc76c7995851b35f6206b1b90470d131e74541
Maxim Fedin <m-fedin@mail.ru>
2020-04-10 23:48:25 +0300

    addClientCerts

--- src/main/scala/org/enterprisedlt/fabric/client/FabricChannel.scala
+++ src/main/scala/org/enterprisedlt/fabric/client/FabricChannel.scala
@@ -1,5 +1,7 @@
 package org.enterprisedlt.fabric.client
 
+import java.util.Properties
+
 import org.enterprisedlt.spec.BinaryCodec
 import org.hyperledger.fabric.sdk.{BlockListener, ChaincodeID, Channel, HFClient}
 
@@ -24,5 +26,12 @@ class FabricChannel(
     //=========================================================================
     def setupBlockListener(listener: BlockListener): Try[String] = Try {
         fabricChannel.registerBlockListener(listener)
+    }
+
+    def addClientCerts(clientCertFile: String, clientKeyFile: String): Unit = {
+        val props = new Properties()
+        props.put("clientCertFile", clientCertFile)
+        props.put("clientKeyFile", clientKeyFile)
+        fabricChannel.setServiceDiscoveryProperties(props)
     }
 }
